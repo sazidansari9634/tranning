20d6329f4b56aa876bc5a7200df88774ef6065ab
Sazid Ansari <93916978+sazidansari9634@users.noreply.github.com>
2021-12-08 06:35:35 +0530

    Update calculator.rb
    using with claas and object

--- ruby/calculator.rb
+++ ruby/calculator.rb
@@ -1,21 +1,38 @@
 
-  puts "Enter the first number:"
-  num1 =gets.chomp().to_f
+ 
+            class Calculator
 
-  puts "Enter the oprator:"
-  operator = gets.chomp()
-  puts "Enter the second number:"
-  num2 =gets.chomp().to_f
-    if operator == "+"
-        print (num1 + num2)
+  def initialize
+    puts "Please Enter First Numbers : "
+    @num1=gets.chomp.to_i
+    puts "Please Type Operation : "
+    @oprt=gets.chomp
+    puts "Please Enter Second Numbers :"
+    @num2=gets.chomp.to_i
 
-        elsif operator == "-"
-            print (num1 - num2)
-            elsif operator == "*"
-                print (num1 * num2)
-                elsif operator == "/"
-                    print (num1 / num2)
-                else
-                    print "Invalid opertor"
-                end
-            
+  end
+
+  def result
+    if @oprt == '+'
+      @num1 + @num2
+
+    elsif @oprt == '-'
+      @num1 - @num2
+
+    elsif @oprt == '*'
+      @num1 * @num2
+
+      elsif @oprt == '/'
+        @num1 / @num2
+
+      else 
+        puts "Something went wrong no , please try again "
+
+      end
+
+    end
+  end
+
+  cal=Calculator.new
+  puts "Result is :"
+  puts cal.result
