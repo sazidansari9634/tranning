7d035e0339bcbd625c4d3997cfadfe9366768361
sazidansari <sazidansari9634@gmail.com>
2021-12-10 06:13:34 +0530

    five example add

--- ruby/oop_practice/inherit.rb
+++ ruby/oop_practice/inherit.rb
@@ -73,3 +73,196 @@ box.printArea()
 # Output -    Big Box area is: 200
 
 
+#Example -1  Ruby program to demonstrate inheritance
+class ClassA
+    def Show
+        puts "Welcome to IncludeHelp"
+    end
+
+    def Message
+        puts "Enter your name: "
+        nm=gets.chomp
+        puts "Hello #{nm} , I hope you are doing greate"
+    end
+end
+
+class ClassB<ClassA
+    def Hello
+        puts "Hello World !"
+    end
+end
+
+obj1=ClassB.new
+obj1.Show
+obj1.Message
+obj1.Hello
+
+=begin 
+Output
+Welcome to IncludeHelp
+Enter your name: 
+sazid
+Hello sazid , I hope you are doing greate
+Hello World !
+=end
+
+#Example-2 This program is makenoise mathod cat and dog
+
+class Animal 
+    def makenoise
+        puts "HurrDurr"
+    end
+end
+
+class Cat < Animal
+    def makenoise
+        puts "Meeaaowwwww"
+    end
+end
+class Dog < Animal
+    def makenoise
+    puts "WoofWoof"
+    end
+end
+
+animal =Animal.new
+animal.makenoise()
+
+animal = Cat.new
+animal.makenoise()
+
+animal= Dog.new
+animal.makenoise()
+
+=begin
+output
+Meeaaowwwww
+WoofWoof
+
+this example declare two classes, Cat and Dog have inherited from animal class   , and providing their own implementation their own implementtation of making 
+Noice method
+cat print Meeaaowwwww and Dog print WoofWoof
+=end
+#Example- 3 Program is a super class member using the super keyword in Ruby
+
+class Animal
+    def makeNoise
+        puts "HurrDurr"
+    end
+end
+
+class Cat < Animal
+    def makeNoise
+        super
+        puts "Meeaaowwwww"
+    end
+end
+animal= Animal.new
+animal.makeNoise()
+animal = Cat.new
+animal.makeNoise()
+
+=begin
+output
+
+HurrDurr
+Meeaaowwwww
+ this exmple we createed a Cat class that subclass Animal class. Cat class calls super class method
+in makeNoice method
+=end
+
+# example 4 - represent the super class  & parent class in ruby
+
+class SuperClassExml
+    # constructor of super class class
+    def initialize
+        puts "This is Super Class"
+    end
+
+    #method of subper class
+    def super_method
+        puts "Method of Super Class"
+    end
+end
+
+#sub class or derived class
+
+class Sub_Example < SuperClassExml
+    # constructor of sub class 
+    def initialize
+        puts "This is a sub class"
+    end
+end
+
+#creating the object of super class
+SuperClassExml.new
+#creating object of sub class
+obj_sub=Sub_Example.new
+
+#calling the method of super class use
+#using the class sub class object
+obj_sub.super_method
+
+=begin
+ output
+
+This is Super Class
+This is a sub class
+Method of Super Class
+
+=end
+
+
+
+
+
+
+# example 5
+
+class Animal 
+    def eat
+        puts "eatting"
+    end
+
+    def sleep
+        puts "sleeping"
+    end
+    def move 
+         "moving"
+    end
+end
+
+class Human < Animal
+    def yell
+        puts "Yelling"
+    end
+    def move
+        puts super + " like a human!"
+    end
+end
+
+class Reccoon < Animal
+    def  move
+        puts super + " like a reccoon"
+    end
+end
+
+human=Human.new
+human.eat
+human.move
+reccoon=Reccoon.new
+reccoon.eat
+reccoon.move
+
+=begin
+#this example is reprenting by there are the class Animal, Human and Reccoon where Human and Reccoon inherit from Animal.
+#the relationship - Human is an Animal and Reccoon is an Animal. Animal is super class and Human and Reccoon is sub class.
+output
+
+eatting
+movinglike a human!
+eatting
+movinglike a reccoon
+
+=end
+
--- ruby/oop_practice/instance.rb
+++ ruby/oop_practice/instance.rb
@@ -34,4 +34,88 @@ Area of the box is 200
 
 =end
 
+#Example 1
+
+# This program any number to add, minus, multiply and divide other number
+
+
+class Calculator
+
+    def plus(number,otherno)
+        number + otherno
+    end
+
+    def minus(number,otherno)
+        number - otherno
+    end
+    def multiply(number, otherno)
+        number * otherno
+    end
+    def divide(number,otherno)
+        number / otherno
+    end
+end
+
+#making object in the class
+
+calculator=Calculator.new
+
+#display object value of the show given result
+puts calculator.plus(2,3)
+puts calculator.minus(2,3)
+puts calculator.multiply(2,3)
+puts calculator.divide(2,3)
+
+=begin
+output
+
+5
+-1
+6
+0
+
+=end
+
+#Example 2
+
+#increament age example
+
+class Baby 
+    #define instance method
+    def getAge(n)
+
+        #instance variable
+        @babyAge=n
+    end
+
+    #defining instance method in increament Age
+
+    def incrementAge() 
+        @babyAge +=1
+    end
+
+    #defining instance method display details
+    def displayDetails()
+        puts "Baby age is : #@babyAge"
+    end
+end
+
+# create an object of class baby
+obj = Baby.new
+#calling the instance method of class baby
+obj.getAge(20)
+obj.displayDetails()
+obj.incrementAge()
+obj.displayDetails()
+
+=begin
+output
+Baby age is : 20
+Baby age is : 21
+=end
+
+
+
+
+
 
